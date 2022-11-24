4085d5548793daa7f857e18410eb40ebb44fd3dc
Stas Magdych <mustas@pm.me>
2022-11-24 19:44:01 +0100

    fuck it, learn to use environment vars

--- .environment_hpc.sh
+++
@@ -1,3 +0,0 @@
-export DATA_PATH_PREFIX=# path to your data on HPC
-export WANDB_KEY=#your wandb api key
-export PATH_TO_VENV=#path to your venv on HPC
--- .environment_local.sh
+++
@@ -1,3 +0,0 @@
-export DATA_PATH_PREFIX=# path to your data
-export WANDB_KEY=#your wandb api key
-export PATH_TO_VENV=#path to your venv
