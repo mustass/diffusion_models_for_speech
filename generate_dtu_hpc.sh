381a7e31ea3eb3ae51b8ea6aefa42f0daa4fb37b
Stas Syrota <stasmagdych@pm.me>
2022-11-29 00:17:29 +0100

    something like this to generate

---
+++ generate_dtu_hpc.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#BSUB -q gpuv100
+#BSUB -gpu "num=1"
+#BSUB -J Generate
+#BSUB -n 1
+#BSUB -W 24:00
+#BSUB -R "rusage[mem=32GB]"
+#BSUB -o logs/%J.out
+#BSUB -e logs/%J.err
+echo
+module load python3/3.9.11
+source path_to_your_venv
+
+wandb login your_wandb_key
+echo "Running script..."
+python3 ./scripts/inference.py --run_name=pretrained_model --device=gpu
