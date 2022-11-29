676cb28e22cae35b4a4c5ca9b2c8a6024113cef6
sandorfoldi <sandorfoldi98@gmail.com>
2022-11-29 20:40:56 +0100

    this runs

---
+++ finetune_dtu_hpc.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#BSUB -q gpuv100
+#BSUB -R "select[gpu32gb]"
+#BSUB -gpu "num=1"
+#BSUB -J DiffWave
+#BSUB -n 1
+#BSUB -W 24:00
+#BSUB -R "rusage[mem=32GB]"
+#BSUB -o logs/%J.out
+#BSUB -e logs/%J.err
+echo
+module load python3/3.9.11
+source ../venv/bin/activate
+
+wandb login a9a49618b7c9a34f36b1f55dfc6e9175e7962060
+echo "Running script..."
+python3 ./scripts/finetune.py datamodule=nst_da_cond model=diffwave_base_pretrained trainer.gpus=1 trainer.accelerator=gpu 
---
+++ scripts/finetune.py
@@ -0,0 +1,97 @@
+import os
+import warnings
+from pathlib import Path
+
+import hydra
+import pytorch_lightning as pl
+import torch
+from hydra.utils import get_original_cwd
+from omegaconf import DictConfig, OmegaConf
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+
+from diffspeak.utils.config_utils import sanity_check
+from diffspeak.utils.technical_utils import load_obj
+from diffspeak.utils.utils import save_useful_info, set_seed
+
+warnings.filterwarnings("ignore")
+
+
+def run(cfg: DictConfig) -> None:
+    """
+    Run pytorch-lightning model
+
+    Args:
+        new_dir:
+        cfg: hydra config
+
+    """
+    set_seed(cfg.training.seed)
+    run_name = os.path.basename(os.getcwd())
+    cfg.callbacks.model_checkpoint.params.dirpath = Path(
+        os.getcwd(), cfg.callbacks.model_checkpoint.params.dirpath
+    ).as_posix()
+    callbacks = []
+    for callback in cfg.callbacks.other_callbacks:
+        if callback.params:
+            callback_instance = load_obj(callback.class_name)(**callback.params)
+        else:
+            callback_instance = load_obj(callback.class_name)()
+        callbacks.append(callback_instance)
+
+    loggers = []
+    if cfg.logging.log:
+        for logger in cfg.logging.loggers:
+            if "experiment_name" in logger.params.keys():
+                logger.params["experiment_name"] = run_name
+            loggers.append(load_obj(logger.class_name)(**logger.params))
+
+    callbacks.append(EarlyStopping(**cfg.callbacks.early_stopping.params))
+    callbacks.append(ModelCheckpoint(**cfg.callbacks.model_checkpoint.params))
+
+    trainer = pl.Trainer(
+        logger=loggers,
+        callbacks=callbacks,
+        **cfg.trainer,
+    )
+
+    cfg.datamodule.path_to_metadata = (
+        Path(get_original_cwd()) / cfg.datamodule.path_to_metadata
+    )  # Could also just give absolute paths
+    sanity_check(cfg)
+
+    dm = load_obj(cfg.datamodule.datamodule_name)(cfg=cfg)
+    dm.setup()
+
+    model = load_obj(cfg.training.lightning_module_name).load_from_checkpoint(
+        checkpoint_path=Path(get_original_cwd())/cfg.model.params.pretrained_model_path, cfg=cfg
+    )
+    trainer.fit(model, dm)
+
+    if cfg.general.save_pytorch_model:
+        if cfg.general.save_best:
+            best_path = trainer.checkpoint_callback.best_model_path  # type: ignore
+            # extract file name without folder
+            save_name = os.path.basename(os.path.normpath(best_path))
+            model = model.load_from_checkpoint(best_path, cfg=cfg, strict=False)
+            model_name = Path(
+                cfg.callbacks.model_checkpoint.params.dirpath,
+                f"best_{save_name}".replace(".ckpt", ".pth"),
+            ).as_posix()
+            torch.save(model.model.state_dict(), model_name)
+        else:
+            os.makedirs("saved_models", exist_ok=True)
+            model_name = "saved_models/last.pth"
+            torch.save(model.model.state_dict(), model_name)
+
+
+@hydra.main(config_path="../configs", config_name="config")
+def run_model(cfg: DictConfig) -> None:
+    os.makedirs("logs", exist_ok=True)
+    print(OmegaConf.to_yaml(cfg))
+    if cfg.general.log_code:
+        save_useful_info(os.path.basename(__file__))
+    run(cfg)
+
+
+if __name__ == "__main__":
+    run_model()
